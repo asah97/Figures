3cbe9868c8fa7e2782ca4e90e38c020bc07f9d14
asah97 <deafmeat@gmail.com>
2017-05-27 12:54:46 +0700

    Сериализация вынесена в отдельный класс

---
+++ WinForms/Serialization.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+//using Figures;
+using Newtonsoft.Json;
+
+namespace WinForms
+{
+    public class Serialization
+    {
+        private JsonSerializer _serializer = new JsonSerializer()
+        {
+            TypeNameHandling = TypeNameHandling.All,
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Include
+        };
+
+        public JsonSerializer SerialAccess
+        {
+            get { return _serializer; }
+            set { _serializer = value; }
+        }
+        
+        
+    }
+}
